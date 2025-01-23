2960cccf32517d309548c101d7bbd40b5e20c10a
Sandro Duarte <57500696+Duasandr@users.noreply.github.com>
2025-01-23 20:33:05 +0000

    Add water particle control.

--- GardenSimulator/Assets/WateringCan/Scripts/Controller.cs
+++ GardenSimulator/Assets/WateringCan/Scripts/Controller.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
 
-public class Controller : MonoBehaviour
+
+namespace WateringCan.Scripts
 {
-   [SerializeField] private AudioClip pickupSound;
-
-   public void OnPickup()
-   {
-      AudioSource.PlayClipAtPoint(pickupSound, transform.position);
-   }
-
-   private void OnTriggerEnter(Collider other)
-   {
-      OnPickup();
-   }
+    public class Controller : MonoBehaviour
+    {
+        [SerializeField] private ParticleSystem activeWaterParticles; 
+        [SerializeField] private ParticleSystem normalWaterParticles;
+        [SerializeField] private ParticleSystem sprayWaterParticles;
+        [SerializeField] private ParticleSystem fullAutoWaterParticles;
+       
+        public void SetNormalMode() => activeWaterParticles = normalWaterParticles;
+        public void SetSprayMode() => activeWaterParticles = sprayWaterParticles;
+        public void SetFullAutoWaterMode() => activeWaterParticles = fullAutoWaterParticles;
+        public void StartWaterParticles() => activeWaterParticles.Play();
+        public void StopWaterParticles() => activeWaterParticles.Stop();
+    }
 }
